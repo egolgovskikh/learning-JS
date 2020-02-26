db3c6f16cdafc57375777644f874ac8c69f50c10
yevtea <egolgovskikh@gmail.com>
2020-02-26 18:45:46 +0300

    task 1

---
+++ task1/index.js
@@ -0,0 +1,22 @@
+let money = prompt("Ваш бюджет на месяц?", '');
+let date = prompt("Введите дату в формате YYYY-MM-DD", '');
+let obligatoryExpensesPerMonth = prompt("Введите обязательную статью расходов в этом месяце", '');
+let howMuchWillCost = prompt("Во сколько обойдется?", '');
+
+let appData = {
+    budget: money,
+    timeData: date,
+    expenses: {
+        [obligatoryExpensesPerMonth] : howMuchWillCost
+    },
+    optionalExpenses: '',
+    income: [],
+    savings: "false"
+};
+
+const budgetPerDay = appData.budget / 30;
+
+alert(budgetPerDay);
+console.log(appData);
+
+12
